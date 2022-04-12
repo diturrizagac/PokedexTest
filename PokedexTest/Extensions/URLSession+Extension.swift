fcba7f51e7f5e8eee5e4d27774636fcead4ed6e5
Diego <diturrizagac@gmail.com>
2022-04-11 21:05:08 -0500

    URLSession extension added to support networkLayer

--- PokedexTest/Extensions/URLSession+Extension.swift
+++ PokedexTest/Extensions/URLSession+Extension.swift
@@ -5,8 +5,27 @@
 //  Created by Diego Raul Iturrizaga Casas on 9/04/22.
 //
 
-import UIKit
+import Foundation
 
-class URLSession_Extension: NSObject {
+protocol NetworkSession {
+    func execute(from urlRequest: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void)
+    func execute(from url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void)
+}
 
+extension URLSession: NetworkSession {
+    func execute(from urlRequest: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        let task  = dataTask(with: urlRequest) { (data, response, error) in
+            completionHandler(data, response, error)
+        }
+        
+        task.resume()
+    }
+    
+    func execute(from url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+        let task  = dataTask(with: url) { (data, response, error) in
+            completionHandler(data, response, error)
+        }
+        
+        task.resume()
+    }
 }
